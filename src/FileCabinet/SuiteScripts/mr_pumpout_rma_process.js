65d61b1b6d297fb0a6056d7b6d1778e9dfb7c97c
raghucrm <raghu@crmexpertsny.com>
2026-07-13 00:20:45 +0530

    testing4

--- src/FileCabinet/SuiteScripts/mr_pumpout_rma_process.js
+++ src/FileCabinet/SuiteScripts/mr_pumpout_rma_process.js
@@ -11,7 +11,7 @@ define([
 
 
     /**************************************************************************
-     * GET INPUT DATA -- get all pmp records extra changes additional changes
+     * GET INPUT DATA -- get all pmp records extra changes additional changes to test workflow
      **************************************************************************/
     function getInputData() {
 
